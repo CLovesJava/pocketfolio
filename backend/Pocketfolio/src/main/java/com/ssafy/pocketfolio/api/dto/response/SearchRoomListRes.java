f2c4e1ad022f3d3515b57c03917176a0dd07d72f
배상현 <counhealer@naver.com>
2024-10-22 12:58:41 +0900

    [Fix] QueryDSL orderBy 문제 해결

--- backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/dto/response/SearchRoomListRes.java
+++ backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/dto/response/SearchRoomListRes.java
@@ -1,6 +1,6 @@
 package com.ssafy.pocketfolio.api.dto.response;
 
-import com.ssafy.pocketfolio.db.view.SearchRoomListView;
+import com.google.gson.annotations.SerializedName;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.*;
@@ -25,7 +25,9 @@ public class SearchRoomListRes {
     @Schema(description = "방 주인 프로필 사진 url")
     private String userProfilePic;
     @Schema(description = "좋아요 수")
-    private Long like;
+    @SerializedName("like")
+    private Long likeCount;
+    // like가 MySQL 예약어라서 QueryDSL에서 orderBy()시에 문제 발생, 이미 클라이언트에서 like로 사용하는 상황이라서 SerializedName으로 해결함. Jackson에서는 @JsonProperty
     @Schema(description = "조회수")
     private Long hit;
     @Schema(description = "좋아요 여부")
@@ -33,17 +35,17 @@ public class SearchRoomListRes {
     @Schema(description = "카테고리 이름")
     private String categoryName;
 
-    public SearchRoomListRes(SearchRoomListView view) {
-        roomSeq = view.getRoomSeq();
-        thumbnail = view.getThumbnail();
-        name = view.getName();
-        userSeq = view.getUserSeq();
-        userName = view.getUserName();
-        userProfilePic = view.getUserProfilePic();
-        like = view.getLike();
-        hit = view.getHit();
-        isLiked = view.getIsLiked() > 0;
-        categoryName = view.getCategoryName();
-    }
+//    public SearchRoomListRes(SearchRoomListView view) {
+//        roomSeq = view.getRoomSeq();
+//        thumbnail = view.getThumbnail();
+//        name = view.getName();
+//        userSeq = view.getUserSeq();
+//        userName = view.getUserName();
+//        userProfilePic = view.getUserProfilePic();
+//        like = view.getLike();
+//        hit = view.getHit();
+//        isLiked = view.getIsLiked() > 0;
+//        categoryName = view.getCategoryName();
+//    }
 
 }
--- backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/db/repository/SearchRepository.java
+++ backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/db/repository/SearchRepository.java
@@ -11,60 +11,59 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 // QueryDSL로 리팩토링 예정
 public interface SearchRepository extends JpaRepository<Room, Long> {
     // 포켓 검색: 시작
-    @Query(value = "select r.room_seq as roomSeq, r.thumbnail, r.name, u.user_seq as userSeq, u.name as userName, u.profile_pic as userProfilePic, " +
-            "(select count(*) from room_like where room_seq = roomSeq) as `like`, " +
-            "(select count(*) from room_hit where room_seq = roomSeq) as hit, " +
-            "(select count(*) from room_like where room_seq = roomSeq and user_seq = :myUserSeq) as isLiked, " +
-            "group_concat(c.name) as categoryName " +
-            "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
-            "left outer join category c on rc.category_seq = c.category_seq " +
-            "where r.name like %:keyword% and c.category_seq in :categories group by r.room_seq " +
-            "order by `like` desc",
-            countQuery = "select count(distinct(r.room_seq)) " +
-                    "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
-                    "left outer join category c on rc.category_seq = c.category_seq " +
-                    "where r.name like %:keyword% and c.category_seq in :categories and :myUserSeq = :myUserSeq",
-            nativeQuery = true)
-    Page<SearchRoomListView> searchRoomOrderByLike(@Param("myUserSeq") Long myUserSeq, @Param("keyword") String keyword, @Param("categories") List<Long> categories, Pageable pageable);
-
-    @Query(value = "select r.room_seq as roomSeq, r.thumbnail, r.name, u.user_seq as userSeq, u.name as userName, u.profile_pic as userProfilePic, " +
-            "(select count(*) from room_like where room_seq = roomSeq) as `like`, " +
-            "(select count(*) from room_hit where room_seq = roomSeq) as hit, " +
-            "(select count(*) from room_like where room_seq = roomSeq and user_seq = :myUserSeq) as isLiked, " +
-            "group_concat(c.name) as categoryName " +
-            "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
-            "left outer join category c on rc.category_seq = c.category_seq " +
-            "where r.name like %:keyword% and c.category_seq in :categories group by r.room_seq " +
-            "order by hit desc",
-            countQuery = "select count(distinct(r.room_seq)) " +
-                    "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
-                    "left outer join category c on rc.category_seq = c.category_seq " +
-                    "where r.name like %:keyword% and c.category_seq in :categories and :myUserSeq = :myUserSeq",
-            nativeQuery = true)
-    Page<SearchRoomListView> searchRoomOrderByHit(@Param("myUserSeq") Long myUserSeq, @Param("keyword") String keyword, @Param("categories") List<Long> categories, Pageable pageable);
-
-    @Query(value = "select r.room_seq as roomSeq, r.thumbnail, r.name, u.user_seq as userSeq, u.name as userName, u.profile_pic as userProfilePic, " +
-            "(select count(*) from room_like where room_seq = roomSeq) as `like`, " +
-            "(select count(*) from room_hit where room_seq = roomSeq) as hit, " +
-            "(select count(*) from room_like where room_seq = roomSeq and user_seq = :myUserSeq) as isLiked, " +
-            "(select count(*) from follow where user_to = u.user_seq) as followerTotal, " +
-            "group_concat(c.name) as categoryName " +
-            "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
-            "left outer join category c on rc.category_seq = c.category_seq " +
-            "where r.name like %:keyword% and c.category_seq in :categories group by r.room_seq " +
-            "order by followerTotal desc",
-            countQuery = "select count(distinct(r.room_seq)) " +
-                    "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
-                    "left outer join category c on rc.category_seq = c.category_seq " +
-                    "where r.name like %:keyword% and c.category_seq in :categories and :myUserSeq = :myUserSeq",
-            nativeQuery = true)
-    Page<SearchRoomListView> searchRoomOrderByFollowerTotal(@Param("myUserSeq") Long myUserSeq, @Param("keyword") String keyword, @Param("categories") List<Long> categories, Pageable pageable);
+//    @Query(value = "select r.room_seq as roomSeq, r.thumbnail, r.name, u.user_seq as userSeq, u.name as userName, u.profile_pic as userProfilePic, " +
+//            "(select count(*) from room_like where room_seq = roomSeq) as `like`, " +
+//            "(select count(*) from room_hit where room_seq = roomSeq) as hit, " +
+//            "(select count(*) from room_like where room_seq = roomSeq and user_seq = :myUserSeq) as isLiked, " +
+//            "group_concat(c.name) as categoryName " +
+//            "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
+//            "left outer join category c on rc.category_seq = c.category_seq " +
+//            "where r.name like %:keyword% and c.category_seq in :categories group by r.room_seq " +
+//            "order by `like` desc",
+//            countQuery = "select count(distinct(r.room_seq)) " +
+//                    "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
+//                    "left outer join category c on rc.category_seq = c.category_seq " +
+//                    "where r.name like %:keyword% and c.category_seq in :categories and :myUserSeq = :myUserSeq",
+//            nativeQuery = true)
+//    Page<SearchRoomListView> searchRoomOrderByLike(@Param("myUserSeq") Long myUserSeq, @Param("keyword") String keyword, @Param("categories") List<Long> categories, Pageable pageable);
+//
+//    @Query(value = "select r.room_seq as roomSeq, r.thumbnail, r.name, u.user_seq as userSeq, u.name as userName, u.profile_pic as userProfilePic, " +
+//            "(select count(*) from room_like where room_seq = roomSeq) as `like`, " +
+//            "(select count(*) from room_hit where room_seq = roomSeq) as hit, " +
+//            "(select count(*) from room_like where room_seq = roomSeq and user_seq = :myUserSeq) as isLiked, " +
+//            "group_concat(c.name) as categoryName " +
+//            "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
+//            "left outer join category c on rc.category_seq = c.category_seq " +
+//            "where r.name like %:keyword% and c.category_seq in :categories group by r.room_seq " +
+//            "order by hit desc",
+//            countQuery = "select count(distinct(r.room_seq)) " +
+//                    "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
+//                    "left outer join category c on rc.category_seq = c.category_seq " +
+//                    "where r.name like %:keyword% and c.category_seq in :categories and :myUserSeq = :myUserSeq",
+//            nativeQuery = true)
+//    Page<SearchRoomListView> searchRoomOrderByHit(@Param("myUserSeq") Long myUserSeq, @Param("keyword") String keyword, @Param("categories") List<Long> categories, Pageable pageable);
+//
+//    @Query(value = "select r.room_seq as roomSeq, r.thumbnail, r.name, u.user_seq as userSeq, u.name as userName, u.profile_pic as userProfilePic, " +
+//            "(select count(*) from room_like where room_seq = roomSeq) as `like`, " +
+//            "(select count(*) from room_hit where room_seq = roomSeq) as hit, " +
+//            "(select count(*) from room_like where room_seq = roomSeq and user_seq = :myUserSeq) as isLiked, " +
+//            "(select count(*) from follow where user_to = u.user_seq) as followerTotal, " +
+//            "group_concat(c.name) as categoryName " +
+//            "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
+//            "left outer join category c on rc.category_seq = c.category_seq " +
+//            "where r.name like %:keyword% and c.category_seq in :categories group by r.room_seq " +
+//            "order by followerTotal desc",
+//            countQuery = "select count(distinct(r.room_seq)) " +
+//                    "from room r inner join user u on r.user_seq = u.user_seq left outer join room_category rc on r.room_seq = rc.room_seq " +
+//                    "left outer join category c on rc.category_seq = c.category_seq " +
+//                    "where r.name like %:keyword% and c.category_seq in :categories and :myUserSeq = :myUserSeq",
+//            nativeQuery = true)
+//    Page<SearchRoomListView> searchRoomOrderByFollowerTotal(@Param("myUserSeq") Long myUserSeq, @Param("keyword") String keyword, @Param("categories") List<Long> categories, Pageable pageable);
     // 포켓 검색: 끝
 
     // 포트폴리오 검색: 시작
